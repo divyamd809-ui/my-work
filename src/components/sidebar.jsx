c81c0bdb2c3d6db1bdbbb85489a6fa56630e3a8e
Divya M D <divyamd809@gmail.com>
2026-02-15 20:18:42 +0530

    added codes

---
+++ src/components/sidebar.jsx
@@ -0,0 +1,11 @@
+export default function Sidebar() {
+  return (
+    <div style={{ width: "200px", background: "#111", color: "white", padding: "20px" }}>
+      <h2>SENTRA SOC</h2>
+      <p>Dashboard</p>
+      <p>Incidents</p>
+      <p>Honeypots</p>
+      <p>Agents</p>
+    </div>
+  );
+}
